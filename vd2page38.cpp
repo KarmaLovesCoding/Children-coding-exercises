78fa0c706da7a0d6be4e103733a7812f5248cfc3
Kamayoshi <149416677+KarmaLovesCoding@users.noreply.github.com>
2024-01-26 21:49:45 +0700

    Add files via upload

---
+++ vd2page38.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	printf ("\n Tieng Beep \a");
+	printf ("\n Doi con tro sang trai 1 ky tu\b");
+	printf ("\n Dau Tab \tva dau backslash \\");
+	printf ("\n Dau nhay don \' va dau nhay kep\"");
+	printf ("\n Dau cham hoi \?");
+	printf("\n Ky tu co ma bat phan 101 la \101");
+	printf ("\n Ky tu co ma thap luc phan 41 la \x041");
+	printf ("\n Dong hien tai,	 xin go Enter");
+	
+	getch();
+	
+	printf ("\r Roi ve dau dong");
+		
+	getch();
+	return 0;
+}
